7f91f5ef5f5326a48db3eb0e7a3dd4cabf69d59a
Iku Iwasa <iku.iwasa@gmail.com>
2019-11-26 07:09:29 +0900

    Implement Iterator over list of AddrRange's

--- src/addr_range.rs
+++ src/addr_range.rs
@@ -1,24 +1,24 @@
 use std::convert::TryFrom;
-use std::ops::Range;
+use std::ops::{Add, Range, Sub};
 
 #[derive(Debug, PartialEq)]
-struct AddrRange<T> {
+pub struct AddrRange<T> {
     pub start: T,
-    pub end: Option<T>,
+    pub end: T,
 }
 
 impl<T> From<Range<T>> for AddrRange<T> {
     fn from(range: Range<T>) -> Self {
         AddrRange {
             start: range.start,
-            end: Some(range.end),
+            end: range.end,
         }
     }
 }
 
 impl<'a, T> TryFrom<&'a str> for AddrRange<T>
 where
-    T: TryFrom<&'a str>,
+    T: Copy + TryFrom<&'a str>,
 {
     type Error = ();
 
@@ -30,23 +30,72 @@ where
                 if let Ok((start, end)) =
                     T::try_from(s1).and_then(|mac1| T::try_from(s2).map(|mac2| (mac1, mac2)))
                 {
-                    return Ok(AddrRange {
-                        start,
-                        end: Some(end),
-                    });
+                    return Ok(AddrRange { start, end });
                 }
             }
         } else {
             let start = T::try_from(value).map_err(|_| ())?;
-            return Ok(AddrRange { start, end: None });
+            return Ok(AddrRange { start, end: start });
         }
         Err(())
     }
 }
 
+struct AddrRanges<T> {
+    ranges: Vec<AddrRange<T>>,
+}
+
+struct AddrRangesIter<T> {
+    ranges: Vec<AddrRange<T>>,
+    offset: i64,
+    done: Vec<bool>,
+}
+
+impl<T> IntoIterator for AddrRanges<T>
+where
+    T: Copy + Ord + Add<u64, Output = T> + Sub<T, Output = i64>,
+{
+    type Item = Vec<T>;
+    type IntoIter = AddrRangesIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let done = self.ranges.iter().map(|_| false).collect();
+        AddrRangesIter::<T> {
+            ranges: self.ranges,
+            offset: 0,
+            done: done,
+        }
+    }
+}
+
+impl<T> Iterator for AddrRangesIter<T>
+where
+    T: Copy + Ord + Add<u64, Output = T> + Sub<T, Output = i64>,
+{
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done.iter().all(|b| *b) {
+            None
+        } else {
+            let mut v = vec![];
+            for range in &self.ranges {
+                let size = range.end - range.start + 1;
+                let offset = (self.offset % size) as u64;
+                if size - 1 <= self.offset {
+                    self.done[v.len()] = true;
+                }
+                v.push(range.start + offset);
+            }
+            self.offset += 1;
+            Some(v)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::AddrRange;
+    use super::{AddrRange, AddrRanges};
     use crate::macaddr::MacAddr;
     use std::convert::TryFrom;
 
@@ -56,7 +105,7 @@ mod tests {
             AddrRange::<MacAddr>::try_from("00:00:00:00:00:00-00:00:00:00:00:10"),
             Ok(AddrRange {
                 start: MacAddr::new(0, 0, 0, 0, 0, 0),
-                end: Some(MacAddr::new(0, 0, 0, 0, 0, 0x10))
+                end: MacAddr::new(0, 0, 0, 0, 0, 0x10)
             })
         );
 
@@ -64,7 +113,7 @@ mod tests {
             AddrRange::<MacAddr>::try_from("10-20"),
             Ok(AddrRange {
                 start: MacAddr::new(0, 0, 0, 0, 0, 0x0a),
-                end: Some(MacAddr::new(0, 0, 0, 0, 0, 0x14))
+                end: MacAddr::new(0, 0, 0, 0, 0, 0x14)
             })
         );
 
@@ -72,7 +121,7 @@ mod tests {
             AddrRange::<MacAddr>::try_from("100-11:22:33:44:55:66"),
             Ok(AddrRange {
                 start: MacAddr::new(0, 0, 0, 0, 0, 0x64),
-                end: Some(MacAddr::new(0x11, 0x22, 0x33, 0x44, 0x55, 0x66))
+                end: MacAddr::new(0x11, 0x22, 0x33, 0x44, 0x55, 0x66)
             })
         );
     }
@@ -83,7 +132,7 @@ mod tests {
             AddrRange::<MacAddr>::try_from("aa:bb:cc:dd:ee:ff"),
             Ok(AddrRange {
                 start: MacAddr::new(0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff),
-                end: None
+                end: MacAddr::new(0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff),
             })
         );
 
@@ -91,7 +140,7 @@ mod tests {
             AddrRange::<MacAddr>::try_from("16"),
             Ok(AddrRange {
                 start: MacAddr::new(0, 0, 0, 0, 0, 0x010),
-                end: None
+                end: MacAddr::new(0, 0, 0, 0, 0, 0x010),
             })
         );
     }
@@ -103,5 +152,79 @@ mod tests {
             Err(())
         );
         assert_eq!(AddrRange::<MacAddr>::try_from("0-1-2"), Err(()));
+    }
+
+    #[test]
+    fn addr_range_ranges_iter_one_element() {
+        let range = AddrRange::<MacAddr>::try_from("1-3").unwrap();
+        let mut ranges_iter = AddrRanges::<MacAddr> {
+            ranges: vec![range],
+        }
+        .into_iter();
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![MacAddr::new(0, 0, 0, 0, 0, 1)])
+        );
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![MacAddr::new(0, 0, 0, 0, 0, 2)])
+        );
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![MacAddr::new(0, 0, 0, 0, 0, 3)])
+        );
+        assert_eq!(ranges_iter.next(), None);
+    }
+
+    #[test]
+    fn addr_range_ranges_iter_3_elements() {
+        let range1 = AddrRange::<MacAddr>::try_from("1-3").unwrap();
+        let range2 = AddrRange::<MacAddr>::try_from("2-6").unwrap();
+        let range3 = AddrRange::<MacAddr>::try_from("7-7").unwrap();
+        let mut ranges_iter = AddrRanges::<MacAddr> {
+            ranges: vec![range1, range2, range3],
+        }
+        .into_iter();
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![
+                MacAddr::new(0, 0, 0, 0, 0, 1),
+                MacAddr::new(0, 0, 0, 0, 0, 2),
+                MacAddr::new(0, 0, 0, 0, 0, 7),
+            ])
+        );
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![
+                MacAddr::new(0, 0, 0, 0, 0, 2),
+                MacAddr::new(0, 0, 0, 0, 0, 3),
+                MacAddr::new(0, 0, 0, 0, 0, 7),
+            ])
+        );
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![
+                MacAddr::new(0, 0, 0, 0, 0, 3),
+                MacAddr::new(0, 0, 0, 0, 0, 4),
+                MacAddr::new(0, 0, 0, 0, 0, 7),
+            ])
+        );
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![
+                MacAddr::new(0, 0, 0, 0, 0, 1),
+                MacAddr::new(0, 0, 0, 0, 0, 5),
+                MacAddr::new(0, 0, 0, 0, 0, 7),
+            ])
+        );
+        assert_eq!(
+            ranges_iter.next(),
+            Some(vec![
+                MacAddr::new(0, 0, 0, 0, 0, 2),
+                MacAddr::new(0, 0, 0, 0, 0, 6),
+                MacAddr::new(0, 0, 0, 0, 0, 7),
+            ])
+        );
+        assert_eq!(ranges_iter.next(), None);
     }
 }
